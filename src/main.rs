7472e4541122dfb7859cb5dacbc1a61a09f3c7a8
Athanasios Kostopoulos <athanasios@akostopoulos.com>
2023-01-16 09:49:00 +0100

    WiP dump looks complete

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,15 @@ use bytes::Bytes;
 use std::env;
 use std::process;
 use std::io::Read;
+//use std::io::Write;
 use std::io::Seek;
 use std::io::SeekFrom;
+use std::io::Cursor;
 use hex;
-//use block_modes::Cbc;
-//use block_modes::BlockMode;//, Cbc};
-//use block_modes::block_padding::Pkcs7;
-use aes::cipher::block_padding::Pkcs7;
-//use aes::Aes256;
-use generic_array::GenericArray;
 use sha2::Sha256;
 use hmac::{Hmac, Mac};
-use aes::cipher::KeyIvInit;
-use aes::cipher::BlockDecryptMut;
+use libaes::Cipher;
+//use std::io::Bytes;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -33,13 +29,56 @@ fn main() {
 
 fn dump(fware: String) {
     println!("Hello from dump");
-    let payload = Vec::new();
-    let version = decrypt(fware, payload);
-    println!("{}", version);//, payload.len());
+    //let payload = Vec::new();
+    let (version, payload) = decrypt(fware);//, payload);
+    println!("{} {}", version, payload.len());
+    let mut product_buf = [0u8; 4];
+    let mut cursor = Cursor::new(payload);
+    let product = cursor.read(&mut product_buf).unwrap() as u32;
+    assert!(product == 0x010000a5,"{} != {}", 0x010000a5, product);
+    /*
+    product, version_again, num_operations, header_size = struct.unpack("<IIII", cursor.read(16))
+    assert product == 0x010000a5
+    assert version_again == version
+    cursor.seek(header_size)
+
+    print(f"Firmware update has {num_operations} operations:")
+
+    for i in range(num_operations):
+        op_size, raw_op_type, op_arg, op_path_size, op_extra_size = struct.unpack("<IIIII", cursor.read(20))
+        op_type = OperationType(raw_op_type)
+        op_path = cursor.read(op_path_size).decode("utf-8")[:-1]
+
+        alignment_remainder = op_path_size % 4
+        if alignment_remainder != 0:
+            alignment_padding = 4 - alignment_remainder
+            cursor.seek(alignment_padding, os.SEEK_CUR)
+        else:
+            alignment_padding = 0
+
+        op_data = cursor.read(op_size - (20 + op_path_size + alignment_padding + op_extra_size))
+
+        print(f"\t{op_type} arg={op_arg} path=\"{op_path}\"")
+
+        if op_type is OperationType.UPDATE_FILE:
+            h = hashes.Hash(hashes.SHA256())
+            h.update(op_data)
+            digest = h.finalize().hex()
+            print(f"\t\tdata=({len(op_data)} bytes, SHA-256: {digest})")
+
+            if output_dir is not None:
+                target_path = output_dir / op_path[1:]
+                target_path.parent.mkdir(parents=True, exist_ok=True)
+                target_path.write_bytes(op_data)
+                print("\t\twritten to:", target_path)
+
+        op_extra_data = cursor.read(op_extra_size)
+
+    assert len(cursor.read()) == 0
+*/
 }
 
-//fn decrypt(fware: String, payload: Vec<u8>) -> (&'static[u8], u32){
-fn decrypt(fware: String, mut payload: Vec<u8>) -> u32 {
+fn decrypt(fware: String) -> (u32, Vec<u8>) {
     println!("Hello from decrypt");
 
     let pubkey_n_raw = Bytes::from_static(&[
@@ -62,11 +101,6 @@ fn decrypt(fware: String, mut payload: Vec<u8>) -> u32 {
     ]);
     let pubkey_n  = num_bigint::BigInt::from_bytes_be(Sign::Plus, &pubkey_n_raw);
     let pubkey_e = num_bigint::BigInt::from_bytes_be(Sign::Plus, &[0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x01 ]);
-    let pkcs1_rsa_sha256_id = Bytes::from_static(&[ 0x30, 0x31, 0x30, 0x0d,
-                                                    0x06, 0x09, 0x60, 0x86, 
-                                                    0x48, 0x01, 0x65, 0x03, 
-                                                    0x04, 0x02, 0x01, 0x05, 
-                                                    0x00, 0x04, 0x20 ]);
     let mut f = File::open(&fware).expect("Failed to open firmware");
     let fsize = fs::metadata(&fware).unwrap().len();
     println!("Firmware size: {}",fsize);
@@ -109,104 +143,43 @@ fn decrypt(fware: String, mut payload: Vec<u8>) -> u32 {
     assert!(encrypted_payload.len() == enc_payload_sz,"actual {} != calculated {}", encrypted_payload.len(), enc_payload_sz);
     let mut raw_signature = Vec::new();
     println!("{}",raw_signature.len()); // 0 - as it should be ...
-    //let old_position = f.seek(SeekFrom::Current(0)).unwrap();
-    //println!("old pos: {}/{}", old_position, fsize);
     f.read_to_end(&mut raw_signature);
     assert!(raw_signature.len() == pubkey_n_raw.len(), "{}/{}/256 {}", raw_signature.len(), pubkey_n_raw.len(), fsize - encrypted_payload.len()as u64);
-    type Aes256CbcDec = cbc::Decryptor<aes::Aes256>;
-    let key = GenericArray::from_slice(aes256_key);
-    let iv =  GenericArray::from_slice(aes256_iv);
-    //let enc_payload: GenericArray<u8, foo> = GenericArray::from_slice(&encrypted_payload);
-    let mut buf = vec![0u8; enc_payload_sz];
-    //type Aes256Cbc = Cbc<Aes256, Pkcs7>;
-    //let cipher = Aes256Cbc::new_var(&aes256_key, &aes256_iv).unwrap();
-    //let payload = cipher.decrypt_vec(&encrypted_payload).unwrap();
     println!("{} {}", encrypted_payload.len(), encrypted_payload.len() % 16);
-    let cipher = Aes256CbcDec::new(&key, &iv);
-    println!("{:#?}", cipher);
-    cipher.decrypt_padded_b2b_mut::<Pkcs7>(encrypted_payload.as_slice(), &mut payload).unwrap(); // UnpadError
-    //let foo = cipher.decrypt_blocks_b2b_mut(&encrypted_payload, &mut buf.into_boxed_slice());//.unwrap();//.to_vec(); // UnpadError
-    //let foo = cipher.decrypt_padded_vec_mut(&encrypted_payload, &mut buf.into_boxed_slice());//.unwrap();//.to_vec(); // UnpadError
-    /*
-        unused_space_in_last_block = payload[-1]
-        block_size = 16
-        assert unused_space_in_last_block <= block_size
-        payload = payload[:-unused_space_in_last_block]
-
-        signature = int.from_bytes(raw_signature, byteorder="big")
-        verify_signature(payload, signature, hmac_key)
-
-        return version, payload
-*/
-    verify_signature(&payload, &raw_signature, hmac_key);
-    return u32::from_be_bytes(version);
+    println!("{} / 32", aes256_key.len());
+    let cipher = Cipher::new_256(aes256_key.try_into().unwrap());
+    let payload = cipher.cbc_decrypt(aes256_iv.try_into().unwrap(), &encrypted_payload[..]).to_vec();
+    // Yeehaw - cbc_decrypt takes care of padding automatically
+    let signature  = num_bigint::BigInt::from_bytes_be(Sign::Plus, &raw_signature);
+    verify_signature(&payload, &raw_signature, hmac_key, pubkey_e, pubkey_n);
+    return (u32::from_le_bytes(version), payload);
 }
-fn verify_signature(data: &[u8], encrypted_sig: &[u8], key: &[u8]) {
+fn verify_signature(data: &[u8], encrypted_sig: &[u8], key: &[u8], e: BigInt, n: BigInt) {
+    let pkcs1_rsa_sha256_id = Bytes::from_static(&[ 0x30, 0x31, 0x30, 0x0d,
+                                                    0x06, 0x09, 0x60, 0x86,
+                                                    0x48, 0x01, 0x65, 0x03,
+                                                    0x04, 0x02, 0x01, 0x05,
+                                                    0x00, 0x04, 0x20 ]);
+
     println!("Hello from verify");
     type HmacSha256 = Hmac<Sha256>;
     let mut mac = HmacSha256::new_from_slice(key).expect("HMAC can take key of any size");
     mac.update(data);
     let digest = mac.finalize().into_bytes();
-    println!("{:#?} {}", digest, digest.len());
+    println!("{} {}", hex::encode(digest), digest.len()*8);
+
+    let padding_needed = 256 - pkcs1_rsa_sha256_id.len() - 3 - digest.len();
+    println!("padding = {}", padding_needed);
+    let mut wrapped = Vec::new();
+    wrapped.push(0x00 as u8);
+    wrapped.push(0x01 as u8);
+    let mut tmp2 = vec![0xffu8; padding_needed];
+    wrapped.append(&mut tmp2);
+    wrapped.push(0x00 as u8);
+    wrapped.append(&mut pkcs1_rsa_sha256_id.to_vec());
+    wrapped.append(&mut digest.to_vec());
+    let expected_sig = num_bigint::BigInt::from_bytes_be(Sign::Plus, &wrapped);
+    let actual_n = num_bigint::BigInt::from_bytes_be(Sign::Plus, &encrypted_sig);
+    let actual_sig = actual_n.modpow(&e, &n);
+    assert!(expected_sig == actual_sig,"expected/actual signature mismatch {} != {}", expected_sig, actual_sig);
 }
-    /*
-    def verify_signature(data, encrypted_signature, key):
-    h = hmac.HMAC(key, hashes.SHA256())
-    h.update(data)
-    digest = h.finalize()
-
-    padding_needed = len(pubkey_n_raw) - (3 + len(pkcs1_rsa_sha256_id) + len(digest))
-    wrapped_digest = bytearray([ 0x00, 0x01 ]) \
-            + (padding_needed * bytearray([ 0xff ])) \
-            + bytearray([ 0x00 ]) \
-            + pkcs1_rsa_sha256_id \
-            + digest
-    expected_signature = int.from_bytes(wrapped_digest, byteorder="big")
-    signature = pow(encrypted_signature, pubkey_e, pubkey_n)
-    assert signature == expected_signature
-*/
-    /*
-def dump(firmware, output_dir):
-    version, payload = decrypt(firmware)
-
-    cursor = io.BytesIO(payload)
-
-    product, version_again, num_operations, header_size = struct.unpack("<IIII", cursor.read(16))
-    assert product == 0x010000a5
-    assert version_again == version
-    cursor.seek(header_size)
-
-    print(f"Firmware update has {num_operations} operations:")
-
-    for i in range(num_operations):
-        op_size, raw_op_type, op_arg, op_path_size, op_extra_size = struct.unpack("<IIIII", cursor.read(20))
-        op_type = OperationType(raw_op_type)
-        op_path = cursor.read(op_path_size).decode("utf-8")[:-1]
-
-        alignment_remainder = op_path_size % 4
-        if alignment_remainder != 0:
-            alignment_padding = 4 - alignment_remainder
-            cursor.seek(alignment_padding, os.SEEK_CUR)
-        else:
-            alignment_padding = 0
-
-        op_data = cursor.read(op_size - (20 + op_path_size + alignment_padding + op_extra_size))
-
-        print(f"\t{op_type} arg={op_arg} path=\"{op_path}\"")
-
-        if op_type is OperationType.UPDATE_FILE:
-            h = hashes.Hash(hashes.SHA256())
-            h.update(op_data)
-            digest = h.finalize().hex()
-            print(f"\t\tdata=({len(op_data)} bytes, SHA-256: {digest})")
-
-            if output_dir is not None:
-                target_path = output_dir / op_path[1:]
-                target_path.parent.mkdir(parents=True, exist_ok=True)
-                target_path.write_bytes(op_data)
-                print("\t\twritten to:", target_path)
-
-        op_extra_data = cursor.read(op_extra_size)
-
-    assert len(cursor.read()) == 0
-*/
